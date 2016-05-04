2a65915c3886b624c3c6cddcef249cd6924a30db
Kevin Nguyen <kevin.nguyen@engineering.ucla.edu>
2016-05-04 15:01:24 -0700

    Add contact form label animation

--- js/logic.js
+++ js/logic.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
         $('#nav-menu ul li a').on('click', function() {
             $('#nav-menu').collapse('hide');
         });
+
+        $('#contactForm').on('input', '.form-group', function(event) {
+            $(this).toggleClass('form-group-content', $(event.target).val().length > 0);
+        }).on('focus', '.form-group', function(event) {
+            $(this).addClass('form-group-focused');
+        }).on('blur', '.form-group', function(event) {
+            $(this).removeClass('form-group-focused');
+        });
 });
